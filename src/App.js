e56ae3951fb1a78dac3c085e8c63aad9df969e7b
kleitos14 <55031486+kleitos14@users.noreply.github.com>
2022-07-23 21:22:25 +0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,328 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div id="landing">
+        <div class="ui secondary stackable menu" id="menu">
+          <div class="item">
+            <i class="industry purple big icon"></i>
+          </div>
+          <div class="right menu">
+            <div class="item header"><a href="index.html">Home</a></div>
+            <div class="item header"><a href="#">Properties</a></div>
+            <div class="item header"><a href="#">About</a></div>
+            <div class="item header"><a href="#">Blog</a></div>
+            <div class="item header"><a href="#">Contact</a></div>
+            <div class="item">
+              <button class="ui huge purple button">Create listing</button>
+            </div>
+          </div>
+        </div>
+        <div class="ui text container">
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <h2 class="ui inverted header">Lorem ipsum dolor sit amet.</h2>
+          <h5 class="ui inverted header">Lorem ipsum dolor sit amet consectetur adipisicing elit. A iste sapiente
+            praesentium blanditiis ea
+            aliquid itaque modi saepe et ad?</h5>
+        </div>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <div class="ui container" id="filter">
+          <div class="ui secondary accordion txt">
+            <button class="ui inverted purple button active title"><i class="filter icon"></i>Find Your
+              Place</button>
+            <div class="content">
+              <div class="ui grid">
+                <div class="doubling five column row">
+                  <div class="column">
+                    <div class="ui selection dropdown">
+                      <i class="dropdown icon"></i>
+                      <div class="text">All Cities</div>
+                      <div class="menu">
+                        <div class="item">Dhaka</div>
+                        <div class="item">Mumbai</div>
+                        <div class="item">Tokyo</div>
+                        <div class="item">Bejing</div>
+                        <div class="item">Dubai</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="column">
+                    <div class="ui selection dropdown">
+                      <i class="dropdown icon"></i>
+                      <div class="text">All Areas</div>
+                      <div class="menu">
+                        <div class="item">Dhanmondi</div>
+                        <div class="item">Gulshan</div>
+                        <div class="item">Uttora</div>
+                        <div class="item">Mirpur</div>
+                        <div class="item">Mohammadpur</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="column">
+                    <div class="ui selection dropdown">
+                      <i class="dropdown icon"></i>
+                      <div class="text">All Types</div>
+                      <div class="menu">
+                        <div class="item">Appartment</div>
+                        <div class="item">Villa</div>
+                        <div class="item">Banglo</div>
+                        <div class="item">Loft</div>
+                        <div class="item">Family Home</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="column">
+                    <div class="ui selection dropdown">
+                      <i class="dropdown icon"></i>
+                      <div class="text">All States</div>
+                      <div class="menu">
+                        <div class="item">Dhaka</div>
+                        <div class="item">Mumbai</div>
+                        <div class="item">Tokyo</div>
+                        <div class="item">Bejing</div>
+                        <div class="item">Dubai</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="column">
+                    <button class="ui purple fluid button ">Search</button>
+                  </div>
+                </div>
+                <div class="doubling five column row">
+                  <div class="column">
+                    <div class="ui selection dropdown">
+                      <i class="dropdown icon"></i>
+                      <div class="text">Beds</div>
+                      <div class="menu">
+                        <div class="item">1</div>
+                        <div class="item">2</div>
+                        <div class="item">3</div>
+                        <div class="item">4</div>
+                        <div class="item">5</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="column">
+                    <div class="ui selection dropdown">
+                      <i class="dropdown icon"></i>
+                      <div class="text">Baths</div>
+                      <div class="menu">
+                        <div class="item">1</div>
+                        <div class="item">2</div>
+                        <div class="item">3</div>
+                        <div class="item">4</div>
+                        <div class="item">5</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="column">
+                    <div class="ui selection dropdown">
+                      <i class="dropdown icon"></i>
+                      <div class="text">Min Area</div>
+                      <div class="menu">
+                        <div class="item">Dhaka</div>
+                        <div class="item">Mumbai</div>
+                        <div class="item">Tokyo</div>
+                        <div class="item">Bejing</div>
+                        <div class="item">Dubai</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="column">
+                    <div class="ui selection dropdown">
+                      <i class="dropdown icon"></i>
+                      <div class="text">Max Area</div>
+                      <div class="menu">
+                        <div class="item">Dhaka</div>
+                        <div class="item">Mumbai</div>
+                        <div class="item">Tokyo</div>
+                        <div class="item">Bejing</div>
+                        <div class="item">Dubai</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="column">
+                    <button class="ui purple fluid button">
+                      <i class="setting icon"></i>
+                      More Options
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="ui container">
+        <br />
+        <br />
+        <br />
+        <br />
+        <h2>Lorem ipsum dolor sit amet consectetur.</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet reprehenderit vitae eligendi, nemo ea ut a et
+          iusto incidunt cupiditate, reiciendis iste atque quidem corporis numquam labore, perferendis voluptas
+          obcaecati quia eaque itaque debitis facere. Natus animi, ea iste earum a ad distinctio tempora modi
+          cupiditate, itaque eum aut quisquam.</p>
+        <br />
+        <br />
+        <br />
+        <div class="ui stackable grid">
+          <div class="two column row">
+            <div class="column">
+              <img src="img/aerial-photography-aerial-shot-aerial-view-1546166.jpg" alt="Image"
+                class="image fluid" />
+              <p class="img-txt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo laboriosam
+                corrupti iure excepturi sed perferendis quisquam nulla maxime enim tempora.</p>
+            </div>
+            <div class="column">
+              <img src="img/Tokyo-bridge-night-buildings-skyscrapers-1058017-wallhere.com.jpg" alt="Image"
+                class="image fluid" />
+              <p class="img-txt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo laboriosam
+                corrupti iure excepturi sed perferendis quisquam nulla maxime enim tempora.</p>
+            </div>
+          </div>
+          <div class="four column row">
+            <div class="column">
+              <img src="img/1920x1080-animal-bird-fauna-kingfisher-nature-1428583-wallhere.com.jpg" alt="Image"
+                class="image fluid" />
+              <p class="img-txt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo laboriosam
+                corrupti iure excepturi sed perferendis quisquam nulla maxime enim tempora.</p>
+            </div>
+            <div class="column">
+              <img src="img/Tokyo-bridge-night-buildings-skyscrapers-1058017-wallhere.com.jpg" alt="Image"
+                class="image fluid" />
+              <p class="img-txt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo laboriosam
+                corrupti iure excepturi sed perferendis quisquam nulla maxime enim tempora.</p>
+            </div>
+            <div class="column">
+              <img src="img/tree-nature-grass-outdoor-branch-bird-239571-pxhere.com.jpg" alt="Image"
+                class="image fluid" />
+              <p class="img-txt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo laboriosam
+                corrupti iure excepturi sed perferendis quisquam nulla maxime enim tempora.</p>
+            </div>
+            <div class="column">
+              <img src="img/hurrican-car-1201296-wallhere.com.jpg" alt="Image"
+                class="image fluid" />
+              <p class="img-txt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo laboriosam
+                corrupti iure excepturi sed perferendis quisquam nulla maxime enim tempora.</p>
+            </div>
+          </div>
+        </div>
+
+        <br />
+        <br />
+        <br />
+        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor, temporibus ut? Dicta nulla ipsam ducimus hic
+          fugit quas, non vitae libero facilis ratione modi, .</p>
+
+        <br /><br />
+        <div class="ui stackable grid">
+          <div class="three column row">
+            <div class="column">
+              <i class="laptop huge icon"></i>
+              <h2 class="ui big header">Fully Responsive</h2>
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora doloribus ab enim cum nobis
+                obcaecati aut, vitae laborum reprehenderit officia?</p>
+            </div>
+            <div class="column">
+              <i class="clock huge icon"></i>
+              <h2 class="ui big header">Powerfull Admin</h2>
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora doloribus ab enim cum nobis
+                obcaecati aut, vitae laborum reprehenderit officia?</p>
+            </div>
+            <div class="column">
+              <i class="world huge icon"></i>
+              <h2 class="ui big header">Multi Language</h2>
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora doloribus ab enim cum nobis
+                obcaecati aut, vitae laborum reprehenderit officia?</p>
+            </div>
+          </div>
+          <div class="three column row">
+            <div class="column">
+              <i class="search huge icon"></i>
+              <h2 class="ui big header">Redius Search</h2>
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora doloribus ab enim cum nobis
+                obcaecati aut, vitae laborum reprehenderit officia?</p>
+            </div>
+            <div class="column">
+              <i class="id badge huge icon"></i>
+              <h2 class="ui big header">Powerfull Admin</h2>
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora doloribus ab enim cum nobis
+                obcaecati aut, vitae laborum reprehenderit officia?</p>
+            </div>
+            <div class="column">
+              <i class="language huge icon"></i>
+              <h2 class="ui big header">Multi Language</h2>
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora doloribus ab enim cum nobis
+                obcaecati aut, vitae laborum reprehenderit officia?</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <br /><br />
+      <br />
+      <div class="ui inverted vertical footer segment">
+        <div class="ui center aligned container">
+          <div class="ui stackable inverted divided grid">
+            <div class="three wide column">
+              <h4 class="ui inverted header">Group 1</h4>
+              <div class="ui inverted link list">
+                <a href="#" class="item">Link One</a>
+                <a href="#" class="item">Link Two</a>
+                <a href="#" class="item">Link Three</a>
+                <a href="#" class="item">Link Four</a>
+              </div>
+            </div>
+            <div class="three wide column">
+              <h4 class="ui inverted header">Group 2</h4>
+              <div class="ui inverted link list">
+                <a href="#" class="item">Link One</a>
+                <a href="#" class="item">Link Two</a>
+                <a href="#" class="item">Link Three</a>
+                <a href="#" class="item">Link Four</a>
+              </div>
+            </div>
+            <div class="three wide column">
+              <h4 class="ui inverted header">Group 3</h4>
+              <div class="ui inverted link list">
+                <a href="#" class="item">Link One</a>
+                <a href="#" class="item">Link Two</a>
+                <a href="#" class="item">Link Three</a>
+                <a href="#" class="item">Link Four</a>
+              </div>
+            </div>
+            <div class="seven wide column">
+              <h4 class="ui inverted header">Footer Header</h4>
+              <p>Extra space for a call to action inside the footer that could help re-engage users.</p>
+            </div>
+          </div>
+          <div class="ui inverted section divider"></div>
+          <img src="img/1920x1080-animal-bird-fauna-kingfisher-nature-1428583-wallhere.com.jpg" class="ui centered mini image" />
+          <div class="ui horizontal inverted small divided link list">
+            <a class="item" href="#">Site Map</a>
+            <a class="item" href="#">Contact Us</a>
+            <a class="item" href="#">Terms and Conditions</a>
+            <a class="item" href="#">Privacy Policy</a>
+          </div>
+        </div>
+      </div>
+    </>
+
+
+  );
+}
+
+export default App;
